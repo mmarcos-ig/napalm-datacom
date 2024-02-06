ff6052ae2effed07c59b1aab0ce131c365e431d8
Manuel Marcos <mmarcos@ignetworks.com>
2024-02-06 15:01:25 +0000

    [mmarcos-ig][development] • hostname property in get_facts() getter now removes "#" from name

--- napalm_datacom/datacom.py
+++ napalm_datacom/datacom.py
@@ -222,7 +222,7 @@ class DatacomDriver(NetworkDriver):
         if len(b) > 0:
             facts['model'] = sub("\s+Model:\s+", "", b[0]).replace("\r","")
 
-        facts['hostname'] = self.device.prompt
+        facts['hostname'] = self.device.prompt.replace("#","")
 
         facts['fqdn'] = ""
 
